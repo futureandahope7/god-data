646d2c53d13866f3503d25196f4137555e6f72a6
Robert Palmer <webmaster@theausraygroup.com.au>
2023-10-24 07:00:33 +1000

    time to simplify

--- useGodData.js
+++ useGodData.js
@@ -10,7 +10,7 @@ class TreeLeaf  {
     }
 }
 
-const goddata = {private: {areas: {}, data: {default: undefined}, instances: {}, rootNode: null, privateDataRefresh: {}, privateDataKnownAs: {}, privateRoughTree: {}, privateDataTree:{}, counter: 0, isNew: false, privateDataActualLast: 'default', privateDataAsync: {}, privateDataId: ['default'],  privateDataLastId:['default'], privateDataLast: 'default'}};
+const goddata = {private: {areas: {}, data: {default: undefined}, instances: {}, rootNode: null, privateDataRefresh: {}, floaters: {}, privateDataKnownAs: {}, privateRoughTree: {}, privateDataTree:{}, counter: 0, isNew: false, privateDataActualLast: 'default', privateDataAsync: {}, privateDataId: ['default'],  privateDataLastId:['default'], privateDataLast: 'default'}};
 
 export const GodPrivateData = ({children}) => {
 
@@ -43,6 +43,46 @@ export const GodPrivateData = ({children}) => {
 const GodDataHead = ({id,lastId, refresh}) => {
 
     goddata.private.privateDataLast = lastId;
+    console.log('START ====================================', id, lastId)
+
+    const checkValues = (theid) => {
+
+        if(typeof goddata.private.privateRoughTree[theid] === 'undefined'){
+            return true;
+        }
+
+        const ok = traverseForward(goddata.private.privateRoughTree[theid]);
+
+        if(ok === false){
+           return false;
+        }
+
+        let anid = theid;
+        while(goddata.private.privateRoughTree[anid].previous !== null){
+            if(goddata.private.privateRoughTree[anid].previous.id == id){
+                return false;
+            }
+            anid = goddata.private.privateRoughTree[anid].previous.id;
+        }
+        return true;
+    }
+
+    const traverseForward = (tree) => {
+        if(tree.id === id){
+            return false;
+        }
+        for(let i in tree.next){
+            if(tree.next.hasOwnProperty(i)){
+                if(traverseForward(i) === false){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    const isOkToAddTree = checkValues(id);
 
     if(goddata.private.rootNode === id){
         goddata.private.privateDataLast = 'default';
@@ -65,19 +105,22 @@ const GodDataHead = ({id,lastId, refresh}) => {
 
     }
 
-    if(goddata.private.privateDataLast !== 'default' || goddata.private.rootNode  === null || goddata.private.rootNode === id) {
-        if(goddata.private.privateDataLast !== id){
-            goddata.private.privateDataTree[id]['parent'] = goddata.private.privateDataLast;
-            let prev = goddata.private.privateRoughTree[goddata.private.privateDataLast];
-            if (prev) {
+    if(isOkToAddTree) {
 
-                prev.next[id] = goddata.private.privateRoughTree[id];
-                goddata.private.privateRoughTree[id].previous = prev;
+        if (goddata.private.privateDataLast !== 'default' || goddata.private.rootNode === null || goddata.private.rootNode === id) {
+            if (goddata.private.privateDataLast !== id) {
+                goddata.private.privateDataTree[id]['parent'] = goddata.private.privateDataLast;
+                let prev = goddata.private.privateRoughTree[goddata.private.privateDataLast];
+                if (prev) {
+
+                    prev.next[id] = goddata.private.privateRoughTree[id];
+                    goddata.private.privateRoughTree[id].previous = prev;
+                }
             }
-        }
 
-    } else {
-        goddata.private.privateDataTree[id]['parent'] = false;
+        } else {
+            goddata.private.privateDataTree[id]['parent'] = false;
+        }
     }
 
 
@@ -118,7 +161,7 @@ const GodDataHead = ({id,lastId, refresh}) => {
     goddata.private.privateDataId.push(id);
     goddata.private.privateDataLast = id;
     goddata.private.privateDataActualLast = id;
-
+console.log('Global Security set ID to', id)
 
     return React.createElement(React.Fragment, {});
 
@@ -127,7 +170,7 @@ const GodDataHead = ({id,lastId, refresh}) => {
 const GodEndPrivateData = ({parentId, lastId}) => {
 
 
-
+    console.log('END ====================================', parentId, lastId)
 
     goddata.private.privateDataId.pop();
 
@@ -169,11 +212,47 @@ const  useGodData = (dat, priv = 'default') =>{
 
     const syncIssue = (pd !== goddata.private.privateDataLast) ? goddata.private.privateDataLast : false;
     const lastId = goddata.private.privateDataLast;
-    const bad = (pd === 'default' || (typeof goddata.private.privateRoughTree[pd] !== 'undefined' && typeof goddata.private.privateRoughTree[pd].next !== 'undefined' && (Object.keys(goddata.private.privateRoughTree[pd].next).map(()=>{ return true}).length > 0) || (goddata.private.privateRoughTree[pd].done && goddata.private.rootNode !== pd)));
+    if(syncIssue !== false && floating){
+        goddata.private.floaters[id] = syncIssue;
+    }
+    const isBad = ()=> {
+        if(pd === 'default') {
+            return true;
+        }
+        if(typeof goddata.private.privateRoughTree[pd] !== 'undefined' && typeof goddata.private.privateRoughTree[pd].next !== 'undefined'){
+            if((Object.keys(goddata.private.privateRoughTree[pd].next).map(()=>{ return true}).length > 0 && goddata.private.rootNode !== pd)){
+                return true;
+            }
+            if(goddata.private.privateRoughTree[pd].done && goddata.private.rootNode !== pd){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    const bad = isBad();
 
 
 
-   // console.log('FLOATING = id:', id, 'doo', doo, 'floating:', floating, 'pd', pd, 'last', goddata.private.privateDataLast, 'tempVal', tempVal, 'refreshParents', refreshParents, 'syncIssue', syncIssue, 'bad', bad);
+    console.log('FLOATING = id:', id, 'doo', doo, 'floating:', floating, 'pd', pd, 'last', goddata.private.privateDataLast, 'tempVal', tempVal, 'refreshParents', refreshParents, 'syncIssue', syncIssue, 'bad', bad);
+
+    const traverseForward = (tree) => {
+
+        if(goddata.private.privateDataTree[tree.id] !== 'undefined') {
+
+            goddata.private.privateDataTree[tree.id]['fn']();
+        }
+        for(let i in tree.next){
+            if(tree.next.hasOwnProperty(i)){
+
+                traverseForward(goddata.private.privateRoughTree[i]);
+
+            }
+        }
+
+    }
 
 
     const searchBack = (privateID, level) => {
@@ -186,7 +265,7 @@ const  useGodData = (dat, priv = 'default') =>{
             return [goddata.private.privateRoughTree[privateID].id];
         } else {
             data = searchBack(goddata.private.privateRoughTree[privateID].previous.id, level+1);
-        return data.concat([goddata.private.privateRoughTree[privateID].id]);
+            return data.concat([goddata.private.privateRoughTree[privateID].id]);
 
         }
 
@@ -211,8 +290,10 @@ const  useGodData = (dat, priv = 'default') =>{
 
         //on creating node children exist, this is a clear sign that we could be floating or adding in a new record
         if(bad){
+            console.log("SET FLOATING ***********************************", id)
             setFloating(true);
             setDoo(false);
+
             setRefreshParents(true);
         } else {
             setFloating(false);
@@ -220,12 +301,51 @@ const  useGodData = (dat, priv = 'default') =>{
         }
     },[]);
 
+    const refreshEveryThing = () => {
+        for(let i in goddata.private.instances){
+            if(goddata.private.instances.hasOwnProperty(i)){
+                for(let j in goddata.private.instances[i]){
+                    if(goddata.private.instances[i].hasOwnProperty(j)){
+                        goddata.private.instances[i][j].map((val)=>{
+                            if(typeof val['fn'] === 'function'){
+                                val['fn']();
+                                return true;
+                            }
+                            return false;
+                        })
+                    }
+                }
+            }
+        }
+
+
+
+
+    }
+
     useEffect(()=>{
 
 
-        if(floating && (syncIssue !== false)){
+        /*if(floating && (syncIssue !== false)){
+
+            console.log("FIX SYnc flaoting +++++++++++++++++++++++++", id)
+
             setFloating(false);
-            setPd(syncIssue);
+            setPd(goddata.private.floaters[id]);
+            setDoo(true);
+            delete goddata.private.floaters[id];
+
+        } /*else if(doingRefresh && pd === lastId){
+            console.log("FIX SYnc flaoting (rushed) -------------", id)
+            setDoingRefresh(false);
+            setFloating(false);
+            setDoo(true);
+        }  */
+
+        if(typeof goddata.private.floaters[id] !== 'undefined'){
+            console.log("FIX  flaoting with var +++++++++++++++++++++++++", id)
+            setFloating(false);
+            setPd(goddata.private.floaters[id]);
             setDoo(true);
 
         }
@@ -233,17 +353,27 @@ const  useGodData = (dat, priv = 'default') =>{
 
 
 
-    });
+    },[typeof goddata.private.floaters[id] !== 'undefined']);
+
+
+    useEffect(()=>{
+        if(!floating){
+            delete goddata.private.floaters[id];
+        }
+    },[floating])
 
     useEffect(()=>{
 
 
-        if(refreshParents){
+        if(refreshParents) {
 
-            let ids = searchBack(pd, 0);
+
+            /*let ids = searchBack(pd, 0);
             if(ids.length > 0) {
 
                 let tempid = ids[0];
+
+                console.log('initial ids' , ids, 'pd is ', pd);
 
                 if (typeof goddata.private.privateDataKnownAs[tempid] !== 'undefined') {
 
@@ -252,25 +382,54 @@ const  useGodData = (dat, priv = 'default') =>{
 
                             let val = goddata.private.privateDataKnownAs[tempid][i];
                             if(val !== pd) {
-                                ids = searchBack(val, 0).concat(ids);
+                                let breakit = false;
+                                let second = [pd];
+                                let start = true;
+                                console.log(ids, 'before', 'pd is ', pd)
+                                while(start || (typeof goddata.private.privateRoughTree[second[0]] !== 'undefined' && goddata.private.privateRoughTree[second[0]].previous !== null && !breakit)) {
+                                    console.log('THE ids', ids);
+                                    start = false;
+                                    second = searchBack(val, 0);
+                                    if (second.length > 0 && (second[0] !== ids[0])) {
+                                        ids = second.concat(ids);
+                                    } else {
+                                        breakit = true;
+                                    }
+                                    console.log('data tree', goddata.private.privateRoughTree);
+                                    console.log(ids, 'after')
+                                }
                             }
                         }
                     }
                 }
 
-
                 for (let i = 0; i < ids.length; i++) {
+                    console.log('doing function', ids[i])
                     goddata.private.privateDataTree[ids[i]]['fn']();
                 }
                 if (ids.indexOf(lastId) === -1 && typeof goddata.private.privateDataTree[lastId] !== 'undefined') {
                     goddata.private.privateDataTree[lastId]['fn']();
                 }
-                setRefreshParents(false);
-                setDoingRefresh(true);
+
+
+            }*/
+            for (let i in goddata.private.privateDataTree){
+                /*if(goddata.private.privateDataTree.hasOwnProperty(i)){
+                    traverseForward(goddata.private.privateRoughTree[i]);
+                }*/
+                if(goddata.private.privateDataTree.hasOwnProperty(i)){
+                    goddata.private.privateDataTree[i]['fn']();
+                }
+
+            }
+            refreshEveryThing();
+            setRefreshParents(false);
+            setDoingRefresh(true);
+
             }
 
 
-        }
+
     }, [refreshParents]);
 
 
@@ -280,45 +439,45 @@ const  useGodData = (dat, priv = 'default') =>{
 
     if(doo) {
 
-       if (typeof goddata.private.instances[pd] === 'undefined') {
+        if (typeof goddata.private.instances[pd] === 'undefined') {
 
-           goddata.private.instances[pd] = {};
-       }
-       if (typeof goddata.private.data[pd] === 'undefined') {
-           goddata.private.data[pd] = {};
-       }
-       if (typeof goddata.private.areas[pd] === 'undefined') {
-           goddata.private.areas[pd] = {};
-       }
+            goddata.private.instances[pd] = {};
+        }
+        if (typeof goddata.private.data[pd] === 'undefined') {
+            goddata.private.data[pd] = {};
+        }
+        if (typeof goddata.private.areas[pd] === 'undefined') {
+            goddata.private.areas[pd] = {};
+        }
 
-       if (typeof goddata.private.instances[pd][priv] === 'undefined') {
+        if (typeof goddata.private.instances[pd][priv] === 'undefined') {
 
-           goddata.private.instances[pd][priv] = [];
-       }
+            goddata.private.instances[pd][priv] = [];
+        }
 
-       if (counter === -1) {
+        if (counter === -1) {
 
 
-           if (typeof dat !== 'undefined' && !(typeof goddata.private.areas[pd] !== 'undefined' && typeof goddata.private.areas[pd][priv] !== 'undefined' && goddata.private.areas[pd][priv] === true)) {
+            if (typeof dat !== 'undefined' && !(typeof goddata.private.areas[pd] !== 'undefined' && typeof goddata.private.areas[pd][priv] !== 'undefined' && goddata.private.areas[pd][priv] === true)) {
 
-               goddata.private.data[pd][priv] = dat;
-               goddata.private.areas[pd][priv] = true;
+                goddata.private.data[pd][priv] = dat;
+                goddata.private.areas[pd][priv] = true;
 
-           } else if (typeof goddata.private.data[pd] === 'undefined' || typeof goddata.private.data[pd][priv] === 'undefined') {
+            } else if (typeof goddata.private.data[pd] === 'undefined' || typeof goddata.private.data[pd][priv] === 'undefined') {
 
-               goddata.private.data[pd][priv] = undefined;
-               goddata.private.areas[pd][priv] = false;
-           }
-       }
+                goddata.private.data[pd][priv] = undefined;
+                goddata.private.areas[pd][priv] = false;
+            }
+        }
 
-   }
+    }
 
     const cleanUp = () =>{
 
 
-       if(typeof goddata.private.instances[pd] === 'undefined'){
-           return false;
-       }
+        if(typeof goddata.private.instances[pd] === 'undefined' || typeof goddata.private.instances[pd][priv] === 'undefined'){
+            return false;
+        }
         let len = goddata.private.instances[pd][priv].length;
         for(let i = 0; i < len; i++){
             if(goddata.private.instances[pd][priv][i]['id'] === id){
@@ -398,7 +557,7 @@ const  useGodData = (dat, priv = 'default') =>{
 
         return [undefined, (val)=>{
             setTempVal(val);
-            }, updateAll];
+        }, updateAll];
     }
 
     updateRefresh();
